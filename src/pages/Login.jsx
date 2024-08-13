b6735d480cda6b4ba39d841b34c7c5ce9e6a961d
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2024-08-14 01:06:34 +0500

    login page completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,80 @@
+import { Link } from "react-router-dom";
+
 function Login() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-  
-  export default Login
-  
+  return (
+    <section className="flex min-h-screen w-full justify-between flex-col-reverse lg:flex-row">
+      <section className="w-full lg:w-[45%] lg:px-[100px] pb-[54px] lg:pb-[182px] flex justify-center items-center">
+        <div className="hidden lg:block flex flex-col gap-[26px] mt-[95px]">
+          <div className="flex flex-col gap-[29px] max-w-[450px]">
+            <div className="flex items-center gap-[15px]">
+              <img src="/logo.svg" className="cursor-pointer" />
+              <h2 className="text-[22px] text-white font-[400] leading-[64px]">
+                Password Manager
+              </h2>
+            </div>
+            <p className="text-[18px] leading-[26px] font-[400] text-white">
+              Login to your account with seed , We do the heavy lifting in a
+              no-nonsense, ad-free, tracker-free, and cloud-free manner. Free
+              and open source.
+            </p>
+          </div>
+          <img className="w-[337px]" src="/loginlock.png" />
+        </div>
+        <img className="block lg:hidden" src="/loginlockv2.svg" />
+      </section>
+      <section className="w-full flex-col items-center lg:w-[55%] bg-[#101E71] rounded-[0px_0px_60px_60px] lg:rounded-[166px_0px_0px_166px] px-[20px] lg:px-[85px] pb-[34px] lg:pb-[182px] flex justify-center">
+        <nav className="block lg:hidden flex justify-center items-center py-[14px] gradient-border">
+          <div className="flex items-center gap-[15px]">
+            <img src="/logov2.svg" className="cursor-pointer" />
+            <h2 className="text-[12px] leading-[36px] font-[400] text-white">
+              Password Manager
+            </h2>
+          </div>
+        </nav>
+        <div className="w-full mt-[37px] lg:mt-[95px] flex flex-col gap-[30px] lg:gap-[42px] max-w-[637px]">
+          <img
+            className="w-[173px] lg:w-[255px] mx-auto"
+            src="/loginmainlogo.svg"
+          />
+          <h3 className="text-white text-center text-[31px] lg:text-[46px] leading-[43px] lg:leading-[64px] font-[400]">
+            Log In
+          </h3>
+          <form className="flex flex-col gap-[42px] lg:gap-[92px]">
+            <div className="flex flex-col gap-[4px]">
+              <label className="text-[#DFDFDF] text-[16px] leading-[32px] font-[400]">
+                Key Seed
+              </label>
+              <input
+                className="border-[1px] rounded-[10px] border-[#28399F] outline-none bg-[#0E1A60] py-[15px] px-[24px] placeholder:text-[#DFDFDF36] text-white text-[16px] leading-[32px] font-[400]"
+                placeholder="Enter your key seed..."
+              />
+            </div>
+            <div className="flex flex-col gap-[14.64px] lg:gap-[32px]">
+              <button
+                className="mx-[auto] bg-[linear-gradient(90deg,_#A143FF_0%,_#5003DB_100%)] py-[10px] 
+              lg:py-[19px] max-w-[312px] w-[100%] rounded-[11.61px] lg:rounded-[18.37px] outline-none 
+              border-none text-[12px] lg:text-[15.5px] leading-[15.26px] 
+              lg:leading-[20.18px] font-[400] text-[#FFFFFF66]"
+              >
+                Next
+              </button>
+              <p className="text-center text-[#DFDFDF] text-[12px] lg:text-[16px] leading-[32px] font-[400]">
+                Don’t you have any account?{" "}
+                <Link className="text-[#A143FF]" to="/auth/register">
+                  {" "}
+                  Register{" "}
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+        <img
+          className="hidden lg:block absolute right-0 top-[-10px]"
+          src="/loginpattern.svg"
+        />
+      </section>
+    </section>
+  );
+}
+
+export default Login;
