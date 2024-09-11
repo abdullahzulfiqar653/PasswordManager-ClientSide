14cc5b86475b5a722082bae4a4de1f9ce27d0139
Warda-Alam <wardaalamaslam@gmail.com>
2024-09-12 02:04:30 +0400

    Navbar for mobile screen updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,14 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <header className="z-10 relative bg-[linear-gradient(352deg,_rgba(14, 26, 96, 0.00)_4.12%,_#0E1A60_72.05%)]">
-      <section className="container">
-        <nav className="flex justify-between items-center py-[11px] gradient-border">
-          <Link to="/" className="flex items-center gap-[15px]">
-            <img src="/logo.svg" className="cursor-pointer w-10 md:w-full" />
-            <h2 className="md:text-[22px] text-[19px] text-white whitespace-nowrap">
+      <section className="md:container ">
+        <nav className="flex justify-around md:justify-between md:items-center py-[11px] gradient-border">
+          <Link to="/" className="flex items-center gap-[6px] md:gap-[15px]">
+            <img src="/logo.svg" className="cursor-pointer w-10 md:w-full ml-4 md:ml-0" />
+            <h2 className="md:text-[22px] xs:font-medium xs:pb-2 text-[16px] text-white whitespace-nowrap">
               Password Manager
             </h2>
           </Link>
@@ -36,59 +32,25 @@ function Navbar() {
               </Link>
             </div>
           </div>
-
-          {!isOpen ? (
+          {/*
             <img
               src="/hamburger.svg"
               className="sm:block md:hidden cursor-pointer"
               onClick={toggleMenu}
               alt="Menu"
             />
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 sm:block md:hidden cursor-pointer"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
-              strokeWidth={2}
-              onClick={toggleMenu}
-              role="button"
-              alt="Close Menu"
-              style={{ border: "1px solid white", borderRadius: "5px" }}
+           "1px solid white", borderRadius: "5px" }}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          )}
+            */}
 
           {/* Mobile menu (Collapsible) */}
-          <div
-            className={`transform ${
-              isOpen ? "scale-100 opacity-100" : "scale-95 opacity-0"
-            } transition-transform transition-opacity ease-out duration-300 absolute z-[999] top-[55px] right-0 bg-[#0b154d] w-full p-5 md:hidden`}
-          >
-            <button className="dm-sans w-full text-left text-[17px]  font-[400] text-white mb-2">
-              Download
-            </button>
-            <Link
-              to="/auth/login"
-              className="dm-sans w-full text-left text-[17px]  font-[400] text-white block mb-2"
-              onClick={() => setIsOpen(false)}
-            >
-              Login
-            </Link>
             <Link
               to="/auth/register"
-              className="dm-sans w-full text-left text-[17px] font-[400] text-white py-[5px] px-[20px] border-[1px] border-white rounded-[15px]"
+              className="dm-sans w-full pt-1 pr-4 pb-2 text-end text-[16px] font-[400] text-white md:hidden"
               onClick={() => setIsOpen(false)}
             >
               Register
             </Link>
-          </div>
         </nav>
       </section>
     </header>
