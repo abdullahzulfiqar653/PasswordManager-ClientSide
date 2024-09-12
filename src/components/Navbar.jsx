1f7bf4c49d5424801dd3102b7d7560fd0e2cf624
Warda-Alam <wardaalamaslam@gmail.com>
2024-09-12 12:28:40 +0400

    Web screen Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
 
   return (
     <header className="z-10 relative bg-[linear-gradient(352deg,_rgba(14, 26, 96, 0.00)_4.12%,_#0E1A60_72.05%)]">
-      <section className="md:container ">
+      <section className="md:container">
         <nav className="flex justify-around md:justify-between md:items-center py-[11px] gradient-border">
           <Link to="/" className="flex items-center gap-[6px] md:gap-[15px]">
             <img src="/logo.svg" className="cursor-pointer w-10 md:w-full ml-4 md:ml-0" />
@@ -14,21 +14,21 @@ function Navbar() {
             </h2>
           </Link>
           <div className="items-center gap-[95px] hidden md:flex">
-            <button className="dm-sans outline-none cursor-pointer border-none text-[20px] leading-[26px] font-[400] text-white">
+            {/* <button className="dm-sans outline-none cursor-pointer border-none text-[20px] leading-[26px] font-[400] text-white">
               Download
-            </button>
+            </button> */}
             <div className="flex items-center gap-[39px]">
               <Link
                 to="/auth/login"
                 className="dm-sans outline-none border-none cursor-pointer text-[20px] leading-[26px] font-[400] text-white"
               >
-                Login{" "}
+                Login
               </Link>
               <Link
                 to="/auth/register"
-                className="dm-sans outline-none text-[20px] border-[1px] cursor-pointer border-[#FFFFFF] py-[22px] px-[40px] rounded-[22px] leading-[26px] font-[400] text-white"
+                className="dm-sans outline-none text-[20px] whitespace-nowrap cursor-pointer border-[#FFFFFF] py-[22px] px-[40px] rounded-[22px] leading-[26px] font-[400] text-white"
               >
-                Register web
+                Register
               </Link>
             </div>
           </div>
