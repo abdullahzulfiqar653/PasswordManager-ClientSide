807e2dc326e09d6d7a3e6f5cef0ab365d513f583
Warda-Alam <wardaalamaslam@gmail.com>
2024-09-13 00:55:32 +0400

    Update register screen for mobile view

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,13 +15,13 @@ function Register() {
   };
 
   return (
-    <section className="flex min-h-screen w-full justify-between flex-col-reverse lg:flex-row">
-      <section className="w-full lg:w-[45%] lg:px-[100px] pb-[54px] lg:pb-[182px] flex justify-center items-center">
-        <div className="hidden lg:flex flex-col gap-[46px] mt-[95px]">
+    <section className="flex min-h-screen w-full justify-between flex-col-reverse md:flex-row">
+      <section className="w-full md:w-[45%] md:px-[100px] pb-[54px] md:pb-[182px] flex justify-center items-center">
+        <div className="hidden md:flex flex-col gap-[46px] mt-[95px]">
           <div className="flex flex-col gap-[29px] max-w-[450px]">
             <Link to="/" className="flex items-center gap-[15px]">
               <img src="/logo.svg" className="cursor-pointer" />
-              <h2 className="text-[22px] text-white font-[400] leading-[64px]">
+              <h2 className="text-[22px] text-white font-[400] leading-[64px] whitespace-nowrap">
                 Password Manager
               </h2>
             </Link>
@@ -33,10 +33,22 @@ function Register() {
           </div>
           <img className="w-[337px]" src="/registerlock.png" />
         </div>
-        <img className="block lg:hidden" src="/loginlockv2.svg" />
+        <img className="block md:hidden z-[1] mr-8 mt-4" src="/registerLocker.svg" />
       </section>
-      <section className="w-full h-[100vh] flex-col items-center lg:w-[55%] bg-[#101E71] rounded-[0px_0px_60px_60px] lg:rounded-[166px_0px_0px_166px] px-[20px]  flex justify-center">
-        <div className="w-full  flex flex-col gap-[30px] lg:gap-[42px] max-w-[637px]">
+      <section className="relative w-full md:h-[100vh] flex-col md:items-center md:w-[55%] bg-[#101E71] rounded-[0px_0px_60px_60px] md:rounded-[166px_0px_0px_166px] px-[20px]  flex md:justify-center">
+      <nav className="md:hidden flex justify-center items-center py-[14px] gradient-border z-[2]">
+          <Link to="/" className="flex items-center gap-[15px]">
+            <img src="/logov2.svg" className="cursor-pointer z-[3]" />
+            <h2 className="text-[12px] leading-[36px] z-[3] font-[400] text-white">
+              Password Manager
+            </h2>
+          </Link>
+        </nav>
+        <img
+            className="absolute w-[100%] md:hidden top-12 left-0 right-0 mx-auto z-[1]"
+            src="/registerLogo.svg"
+          />
+        <div className="w-full z-[3] h-[75vh] flex flex-col gap-[30px] md:gap-[42px] max-w-[637px]">
           {!isAgree ? (
             <RegisterInstruction
               agreementHandler={agreementHandler}
@@ -46,7 +58,7 @@ function Register() {
             <RegistrationSeed seedsData={seedsData} />
           )}
           <img
-            className="hidden lg:block absolute right-0 top-[-10px]"
+            className="hidden md:block absolute right-0 top-[-10px]"
             src="/loginpattern.svg"
           />
         </div>
